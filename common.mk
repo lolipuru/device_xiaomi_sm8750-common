1a94d2eb4ce6e665765603f89b7779f616dfa517
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-03-19 17:48:37 +0100

    sm8750-common: Use night_charging node for Lineage Health HAL
     * apparently xiaomi is using this node for their charging limiter feature as well
    
    Change-Id: I4455caa97a516d709b35c9606da13d649ec5e389

--- common.mk
+++ common.mk
@@ -203,7 +203,7 @@ PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     $(LOCAL_PATH)/configs/linker.config.json
 
 # Lineage Health
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/night_charging)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 
