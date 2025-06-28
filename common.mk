182e0b79ba99943caf2cf78b3598a75606d74481
lolipuru <banstellazz@gmail.com>
2025-06-28 16:56:48 +0700

    sm8750-common: import some init from stock
    Change-Id: I80cdb6eee96359775346d54265932fd5d0a2b24f
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- common.mk
+++ common.mk
@@ -215,10 +215,13 @@ PRODUCT_PACKAGES += \
 
 # Rootdir
 PRODUCT_PACKAGES += \
-    fstab.qcom
+    fstab.qcom \
+    init.recovery.qcom.rc \
+    init.target.rc \
+    ueventd.qcom.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # Sensors
 PRODUCT_COPY_FILES += \
