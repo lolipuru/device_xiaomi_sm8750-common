2f420fa47b7d5cfaf9abe371f441dffa98c39e96
lolipuru <banstellazz@gmail.com>
2025-06-28 16:50:15 +0700

    sm8750-common: import sun manifest from LA.VENDOR.15.4.0.r1-17900-pakala.0 and OS2.0.206.0.VOCCNXM
    Change-Id: I6da2323eb8d663c43617745f8eaa9a476e088b9d
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -147,15 +147,18 @@ BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/configs/vintf/compatibility_matrix.device.xml \
     $(COMMON_PATH)/configs/vintf/compatibility_matrix.xiaomi.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MATRIX_FILE := \
+    $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml \
     hardware/qcom-caf/common/compatibility_matrix.xml
 
 DEVICE_MANIFEST_SKUS := sun
 DEVICE_MANIFEST_SUN_FILES := \
     $(COMMON_PATH)/configs/vintf/manifest_xiaomi.xml \
+    $(COMMON_PATH)/configs/vintf/manifest_pineapple.xml \
     hardware/qcom-caf/sm8750/audio/primary-hal/configs/common/manifest_non_qmaa.xml \
     hardware/qcom-caf/sm8750/audio/primary-hal/configs/common/manifest_non_qmaa_extn.xml
 
