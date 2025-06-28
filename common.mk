82c7d389cfb1dce696ea80c500a01d90aca5fa66
lolipuru <banstellazz@gmail.com>
2025-06-28 17:55:44 +0700

    temp
    testing new stacks, these are no longer necessary
    
    Change-Id: Ic41910bca5e4c8aa1fb445b7126ea56c7122bcbf

--- common.mk
+++ common.mk
@@ -143,8 +143,6 @@ PRODUCT_PACKAGES += \
     mapper.qti \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer3-V1-ndk.vendor \
-    vendor.qti.hardware.display.composer-service.rc \
-    vendor.qti.hardware.display.composer-service.xml \
     vendor.qti.hardware.display.demura-service
 
 PRODUCT_COPY_FILES += \
