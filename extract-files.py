LineageOS/android_device_oneplus_sm8750-common

sm8750-common: Patch some blobs to depend on libtinyxml2-v34.so
libtinyxml2 10.1.0 update which landed in BP4A contains ABI
incompatible changes which cause runtime crashes.

Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

extract-files.py
@@ -57,6 +57,25 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff': blob_fixup()
         .add_needed('libshims_aidl_fingerprint_v3.oplus.so'),
+    (
+        'odm/bin/touchDaemon',
+        'odm/bin/hw/vendor-oplus-hardware-touch-V2-service',
+        'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff',
+        'vendor/bin/poweropt-service',
+        'vendor/lib64/hw/libaudioeffecthal.qti.so',
+        'vendor/lib64/soundfx/libquasar.so',
+        'vendor/lib64/libaodoptfeature.so',
+        'vendor/lib64/libapengine.so',
+        'vendor/lib64/libcamerapoweroptfeature.so',
+        'vendor/lib64/liblearningmodule.so',
+        'vendor/lib64/libgamepoweroptfeature.so',
+        'vendor/lib64/libpowercore.so',
+        'vendor/lib64/liboffscreenpoweroptfeature.so',
+        'vendor/lib64/libpsmoptfeature.so',
+        'vendor/lib64/libstandbyfeature.so',
+        'vendor/lib64/libvideooptfeature.so',
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'odm/etc/init/init.network.rc': blob_fixup()
         .regex_replace(r'/\* (Huo\.Chen@SYSTEM\.RF, 2024/09/06, Add for ICC) \*/', r'# \1'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()

lolipuru/device_xiaomi_sm8750-common

extract-files.py
@@ -82,10 +82,17 @@ blob_fixups: blob_fixups_user_type = {
             'android.hardware.common-V2-ndk.so'
         ),
     (
-        'vendor/lib64/libapengine.so',
         'vendor/lib64/libqti-perfd.so',
     ): blob_fixup()
         .replace_needed('vendor.qti.hardware.display.config-V5-ndk.so', 'vendor.qti.hardware.display.config-V12-ndk.so'),
+    (
+        'vendor/lib64/libapengine.so',
+    ): blob_fixup()
+        .replace_needed('vendor.qti.hardware.display.config-V5-ndk.so', 'vendor.qti.hardware.display.config-V12-ndk.so')
+        .replace_needed(
+            'libtinyxml2.so',
+            'libtinyxml2-v34.so'
+        ),
      (
        'odm/bin/hw/vendor.xiaomi.hw.touchfeature-service',
        'odm/lib64/hw/displayfeature.default.so',
@@ -108,12 +115,27 @@ blob_fixups: blob_fixups_user_type = {
             'android.hardware.sensors-V2-ndk.so',
             'android.hardware.sensors-V3-ndk.so'
         ),
+     (
+       'odm/lib64/hw/displayfeature.default.so',
+     ): blob_fixup()
+        .replace_needed(
+            'android.hardware.sensors-V2-ndk.so',
+            'android.hardware.sensors-V3-ndk.so'
+        )
+        .replace_needed(
+            'libtinyxml2.so',
+            'libtinyxml2-v34.so'
+        ),
     (
        'vendor/bin/hw/vendor.qti.hardware.display.composer-service',
     ): blob_fixup()
         .replace_needed(
             'vendor.qti.hardware.display.composer3-V1-ndk.so',
             'vendor.qti.hardware.display.composer3-V3-ndk.so'
+        )
+        .replace_needed(
+            'libtinyxml2.so',
+            'libtinyxml2-v34.so'
         ),
     (
        'vendor/lib64/libqcodec2_core.so',
@@ -138,6 +160,10 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed(
             'android.hardware.sensors-V2-ndk.so',
             'android.hardware.sensors-V3-ndk.so'
+        )
+        .replace_needed(
+            'libtinyxml2.so',
+            'libtinyxml2-v34.so'
         ),
      (
        'odm/lib64/libqc_hal.so',
@@ -146,6 +172,27 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed(
             'android.hardware.biometrics.fingerprint-V5-ndk.so',
             'android.hardware.biometrics.fingerprint-V4-ndk.so'
+        ),
+     (
+       'odm/lib64/libmiXmlParser.so',
+       'vendor/bin/hw/audiohalservice.qti',
+       'vendor/bin/poweropt-service',
+       'vendor/lib64/libaodoptfeature.so',
+       'vendor/lib64/libaudiocloudctrl.so',
+       'vendor/lib64/libcamerapoweroptfeature.so',
+       'vendor/lib64/libgamepoweroptfeature.so',
+       'vendor/lib64/liblearningmodule.so',
+       'vendor/lib64/liboffscreenpoweroptfeature.so',
+       'vendor/lib64/libpowercallback.so',
+       'vendor/lib64/libpowercore.so',
+       'vendor/lib64/libpsmoptfeature.so',
+       'vendor/lib64/libsdmclient.so',
+       'vendor/lib64/libstandbyfeature.so',
+       'vendor/lib64/libvideooptfeature.so',
+     ): blob_fixup()
+        .replace_needed(
+            'libtinyxml2.so',
+            'libtinyxml2-v34.so'
         ),
 }  # fmt: skip
 